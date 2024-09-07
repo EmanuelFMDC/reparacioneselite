1ab07f2917a8a4422d1ffd12e29d3f6268ceb44d
Emanuel FMDC <emanuel2303e@gmail.com>
2024-09-07 11:38:24 -0600

    botones flotantes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: [
+    './index.html',
+    './src/**/*.js', // Incluye cualquier archivo JS que contenga clases de Tailwind
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0583a5', // Agregamos el color personalizado bajo el nombre 'primary'
+      },
+    },
   },
   plugins: [],
 }
